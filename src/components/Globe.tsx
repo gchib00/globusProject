854d30ff0dbb5630dec8227bfa4c90d80f88e69d
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-04-16 21:12:31 +0200

    added texture to the globe, added orbit controls

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,32 +1,36 @@
 import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 import earth from './earth.jpg';
+// import earth from './earth2';
+// import earth from './world.svg';
 
 export const Globe = () => {
   const canvasRef: any = useRef();
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 1000 );
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
-    camera.position.z = 1;
+    const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+      antialias: true
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    const sphere = new THREE.Mesh(
-      new THREE.SphereGeometry(5, 50, 50),
-      new THREE.MeshBasicMaterial({
-      map: new THREE.TextureLoader().load(earth)
-    }))
-
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const geometry = new THREE.SphereGeometry(5, 50, 50);
+    const material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(earth) });
+    const sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
     camera.position.z = 10;
 
     const animate = () => {
       requestAnimationFrame(animate);
+      // sphere.rotation.x += 0.004
+      // sphere.rotation.y += 0.004
       renderer.render(scene, camera);
     }
     animate();
   }, []);
-
   return (
     <canvas ref={canvasRef} />
   )
