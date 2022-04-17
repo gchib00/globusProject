ba3bccb6733d4460753872c5e6ab8b0c4b84697e
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-04-17 23:06:13 +0200

    added ambient light + a light source that mimics the sun

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-import earth from './earth.jpg';
-// import earth from './earth2';
-// import earth from './world.svg';
+import earth from '../static/earth.jpg';
+import earthRelief from '../static/earthRelief.jpg';
+import earthSpec from '../static/earthSpec.jpg';
 
 export const Globe = () => {
   const canvasRef: any = useRef();
@@ -17,11 +16,29 @@ export const Globe = () => {
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     const controls = new OrbitControls(camera, renderer.domElement);
-    const geometry = new THREE.SphereGeometry(5, 50, 50);
-    const material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(earth) });
-    const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
     camera.position.z = 10;
+    //load textures:
+    const earthMainLayer = new THREE.TextureLoader().load(earth);
+    const earthReliefLayer = new THREE.TextureLoader().load(earthRelief);
+    const earthSpecLayer = new THREE.TextureLoader().load(earthSpec);
+    //create a globe:
+    const geometry = new THREE.SphereGeometry(5, 50, 50);
+    const material = new THREE.MeshPhongMaterial({
+      map: earthMainLayer,
+      bumpMap: earthReliefLayer,
+      bumpScale: 0.07,
+      specularMap: earthSpecLayer,
+      specular: new THREE.Color('grey')
+    });
+    const globe = new THREE.Mesh(geometry, material);
+    scene.add(globe);
+    //create a light source: (MeshPhongMaterial needs a light source, otherwise it will be invisible)
+    const lightSource = new THREE.DirectionalLight("grey", 0.5);
+    lightSource.position.set(0, 3, 1);
+    lightSource.target.position.set(0, 0, 0);
+    scene.add(lightSource);
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+    scene.add(ambientLight);
     //rendering functions:
     const windowResize = () => {
       //readjust the window size if user changes the window(in order to always keep aspect ratio ideal)
