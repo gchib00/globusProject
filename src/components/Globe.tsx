1e619adc4a88679f38a5bb1bbf91da5c51588070
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-04-18 23:13:51 +0200

    added clouds object to the scene

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,11 +1,42 @@
 import React, { MutableRefObject, useEffect, useRef } from 'react'
+import * as THREE from 'three';
 import generateGlobe from './GlobeObjectGeneration/globe';
+import generateClouds from './GlobeObjectGeneration/clouds';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export const Globe = () => {
   const canvasRef = useRef() as MutableRefObject<HTMLCanvasElement>;
+
   useEffect(() => {
-    generateGlobe(canvasRef);
+    //create scene:
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+      antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    camera.position.z = 10;
+    //add objects to the scene:
+    generateGlobe(scene);
+    generateClouds(scene);
+    //rendering functions:
+    const windowResize = () => {
+      //readjust the window size if user changes the window(in order to always keep aspect ratio ideal)
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+      controls.update();
+    }
+    window.addEventListener('resize', windowResize, false);
+    animate();
   }, []);
+
   return (
     <canvas ref={canvasRef} />
   )
--- src/components/GlobeObjectGeneration/globe.ts
+++ src/components/GlobeObjectGeneration/globe.ts
@@ -1,26 +1,15 @@
-import { MutableRefObject } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import earth from '../../static/earth.jpg';
 import earthRelief from '../../static/earthRelief.jpg';
 import earthSpec from '../../static/earthSpec.jpg';
 
-const createGlobe = (canvasRef: MutableRefObject<HTMLCanvasElement>) => {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvasRef.current,
-    antialias: true
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  const controls = new OrbitControls(camera, renderer.domElement);
-  camera.position.z = 10;
+const createGlobe = (scene: THREE.Scene) => {
   //load textures:
   const earthMainLayer = new THREE.TextureLoader().load(earth);
   const earthReliefLayer = new THREE.TextureLoader().load(earthRelief);
   const earthSpecLayer = new THREE.TextureLoader().load(earthSpec);
   //create a globe:
-  const geometry = new THREE.SphereGeometry(5, 50, 50);
+  const geometry = new THREE.SphereGeometry(5, 32, 32);
   const material = new THREE.MeshPhongMaterial({
     map: earthMainLayer,
     bumpMap: earthReliefLayer,
@@ -37,20 +26,6 @@ const createGlobe = (canvasRef: MutableRefObject<HTMLCanvasElement>) => {
   scene.add(lightSource);
   const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
   scene.add(ambientLight);
-  //rendering functions:
-  const windowResize = () => {
-    //readjust the window size if user changes the window(in order to always keep aspect ratio ideal)
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-  const animate = () => {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    controls.update();
-  }
-  window.addEventListener('resize', windowResize, false);
-  animate();
 }
 
 export default createGlobe;
