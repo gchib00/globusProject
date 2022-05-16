2899507a0cd3f3b95204aaf2b788466b8ed0a4bf
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-05-16 22:33:11 +0200

    connected brightness settings to navbar menu

--- src/components/Dashboard/ItemSettings/BrightnessSettings.tsx
+++ src/components/Dashboard/ItemSettings/BrightnessSettings.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeGlobeBrightness } from '../../../store/actions';
 import { State } from '../../../types';
 
 export const BrightnessSettings = () => {
   const brightnessLvl = useSelector((state: State) => state.globeSettings.brightness);
+  const dispatch = useDispatch();
   return (
     <div className='m-radio-btns-contaner'>
-      <label className='m-radio-btn'>
+      <label className='m-radio-btn' onClick={ () => dispatch(changeGlobeBrightness(0)) }>
         <input
           type='radio'
           checked={ brightnessLvl === 0 ? true : false }
         />
         <span>Low</span>
       </label>
-      <label className='m-radio-btn'>
+      <label className='m-radio-btn' onClick={ () => dispatch(changeGlobeBrightness(1)) }>
         <input
           type='radio'
           checked={ brightnessLvl === 1 ? true : false }
         />
         <span>Medium</span>
       </label>
-      <label className='m-radio-btn'>
+      <label className='m-radio-btn' onClick={ () => dispatch(changeGlobeBrightness(2)) }>
         <input
           type='radio' 
           checked={ brightnessLvl === 2 ? true : false }
